90b60e1a7e8bdf84bbcccf32e3da4a41ce467e09
sungang <1120_sungang@gmail.com>
2016-10-13 16:44:10 +0800

    增加demo

--- spring-boot-chapter1/src/main/java/com/sung/Chapter1Application.java
+++ spring-boot-chapter1/src/main/java/com/sung/Chapter1Application.java
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-@EnableAutoConfiguration
 public class Chapter1Application {
 
     public static void main(String[] args) {
